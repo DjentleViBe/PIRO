103560eb27f69e93d6aab7cc0de195d8dca77ebe
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-21 03:27:03 +0200

    pre process

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include "../dependencies/include/preprocess.hpp"
+#include "../dependencies/include/inireader.hpp"
+#include "../dependencies/include/datatypes.hpp"
+
+Giro::MeshParams MP;
 
 int preprocess() {
     std::cout << "This is the preprocess source" << std::endl;
+
+    IniReader reader("setup.ini");
+    // Print all sections and key-value pairs
+    // reader.print();
+    
+    MP.meshtype = std::stoi(reader.get("PreProcess", "MeshType", "default_value"));
+    MP.n[0] = std::stoi(reader.get("PreProcess", "nx", "default_value"));
+    MP.n[1] = std::stoi(reader.get("PreProcess", "ny", "default_value"));
+    MP.n[2] = std::stoi(reader.get("PreProcess", "nz", "default_value"));
+
+    MP.l[0] = std::stoi(reader.get("PreProcess", "lx", "default_value"));
+    MP.l[1] = std::stoi(reader.get("PreProcess", "ly", "default_value"));
+    MP.l[2] = std::stoi(reader.get("PreProcess", "lz", "default_value"));
+
     return 0;
 }
