824ec4d2683e0172ccc25515523073cd3f1e0b10
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-21 03:26:39 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#include "../dependencies/include/init.hpp"
 #include "../dependencies/include/preprocess.hpp"
 #include "../dependencies/include/postprocess.hpp"
 
 int main()
-{
+{	
+	init();
 	preprocess();
 	postprocess();
 	return 0;
