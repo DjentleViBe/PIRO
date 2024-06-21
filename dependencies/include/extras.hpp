eaa04176abe8696ee2f760c35158c008f6fd71d3
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-22 01:03:32 +0200

    extras header

--- dependencies/include/extras.hpp
+++ dependencies/include/extras.hpp
@@ -2,8 +2,18 @@
 #define extras_hpp
 
 #include <string>
+#include <vector>
 
 int writefile(std::string file_path, std::string line_to_write);
 int create_directory(std::string directoryname);
 int get_exec_directory();
+std::vector<std::string> splitString(const std::string& str, char delimiter);
+std::vector<int> convertStringVectorToInt(const std::vector<std::string>& stringVector);
+std::vector<float> convertStringVectorToFloat(const std::vector<std::string>& stringVector);
+std::vector<std::string> floatVectorToString(const std::vector<float>& floatVector);
+std::string concatenateStrings(const std::vector<std::string>& strVector);
+int countSpaces(const std::string& str);
+std::string concatenateStrings2(const std::vector<std::string>& strVector);
+// void printMatrix(const vector<vector<float>>& matrix);
+
 #endif
