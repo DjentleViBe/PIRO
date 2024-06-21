522e5c88f23e7f3b01035c091bb7f6364177ac1b
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-21 03:26:34 +0200

    init

---
+++ src/init.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <filesystem>
+#include "../dependencies/include/init.hpp"
+#include "../dependencies/include/extras.hpp"
+
+std::filesystem::path current_path;
+
+void init(){
+    /*try {
+        // Get the current path
+        current_path = std::filesystem::current_path();
+        
+        // Print the current path
+        std::cout << "Current working directory: " << current_path << std::endl;
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }*/
+    get_exec_directory();
+    create_directory(current_path.string() + "/" + "mesh");
+
+}
