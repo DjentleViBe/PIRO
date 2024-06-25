a72309b4eb575f4b11d6a39eb57e643560e7bf92
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-26 00:53:16 +0200

    print vector header

--- dependencies/include/extras.hpp
+++ dependencies/include/extras.hpp
@@ -15,5 +15,6 @@ std::string concatenateStrings(const std::vector<std::string>& strVector);
 int countSpaces(const std::string& str);
 std::string concatenateStrings2(const std::vector<std::string>& strVector);
 void printMatrix(const std::vector<std::vector<float>>& matrix);
+void printVector(const std::vector<float>& vec);
 
 #endif
