65ac10c7521d80be58234da5285c3dacfa89e02e
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-25 22:53:09 +0200

    operator headers

--- dependencies/include/operatoroverload.hpp
+++ dependencies/include/operatoroverload.hpp
@@ -7,4 +7,10 @@
 
 std::vector<float> operator+(const std::vector<float> &v1, const std::vector<float> &v2);
 std::vector<std::vector<float>> operator+(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+std::vector<float> operator-(const std::vector<float> &v1, const std::vector<float> &v2);
+std::vector<std::vector<float>> operator-(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+std::vector<float> operator*(const std::vector<float> &v1, const std::vector<float> &v2);
+std::vector<std::vector<float>> operator*(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+std::vector<float> operator/(const std::vector<float> &v1, const std::vector<float> &v2);
+std::vector<std::vector<float>> operator/(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
 #endif // OPERATOR_OVERLOAD_HPP
