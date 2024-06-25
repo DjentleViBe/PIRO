9f7c37673d740bfea1789a28bc503e8a310bed5d
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-25 22:15:10 +0200

    operators header

---
+++ dependencies/include/operatoroverload.hpp
@@ -0,0 +1,10 @@
+// operator_overload.hpp
+
+#ifndef OPERATOROVERLOADHPP
+#define OPERATOROVERLOADHPP
+
+#include <vector>
+
+std::vector<float> operator+(const std::vector<float> &v1, const std::vector<float> &v2);
+std::vector<std::vector<float>> operator+(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+#endif // OPERATOR_OVERLOAD_HPP
