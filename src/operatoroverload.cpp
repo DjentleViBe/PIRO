c5d09c0a357b05773b57529ec3598b1ba91f1ea5
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-25 22:52:49 +0200

    other operators

--- src/operatoroverload.cpp
+++ src/operatoroverload.cpp
@@ -12,3 +12,32 @@ std::vector<std::vector<float>> operator+(const std::vector<std::vector<float>> 
     return addM.addMatrices(v1, v2);
 }
 
+std::vector<float> operator-(const std::vector<float> &v1, const std::vector<float> &v2) {
+    Giro::MathOperations sub;
+    return sub.subtractVectors(v1, v2);
+}
+
+std::vector<std::vector<float>> operator-(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2) {
+    Giro::MathOperations subM;
+    return subM.subtractMatrices(v1, v2);
+}
+
+std::vector<float> operator*(const std::vector<float> &v1, const std::vector<float> &v2) {
+    Giro::MathOperations mul;
+    return mul.multiplyVectors(v1, v2);
+}
+
+std::vector<std::vector<float>> operator*(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2) {
+    Giro::MathOperations mulM;
+    return mulM.multiplyMatrices(v1, v2);
+}
+
+std::vector<float> operator/(const std::vector<float> &v1, const std::vector<float> &v2) {
+    Giro::MathOperations div;
+    return div.divideVectors(v1, v2);
+}
+
+std::vector<std::vector<float>> operator/(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2) {
+    Giro::MathOperations divM;
+    return divM.divideMatrices(v1, v2);
+}
