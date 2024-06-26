675e745166d974f1910b9d25ea74e8a02564baef
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-27 00:33:12 +0200

    matrix header

--- dependencies/include/preprocess.hpp
+++ dependencies/include/preprocess.hpp
@@ -2,8 +2,11 @@
 #define preprocess_hpp
 
 #include "datatypes.hpp"
+#include <vector>
 
 extern Giro::MeshParams MP;
+extern std::vector<std::vector<float>> scadivmatrix, vecdivmatrix, scalapmatrix, veclapmatrix;
+ 
 int preprocess();
 
 #endif
