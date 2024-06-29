8d648cfbd80b938c1bbb6a75cbd0ea2293b34a8f
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-29 02:32:23 +0200

    post process

--- src/postprocess.cpp
+++ src/postprocess.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <cstdarg>
 #include "../dependencies/include/postprocess.hpp"
 #include "../dependencies/include/preprocess.hpp"
+#include "../dependencies/include/solve.hpp"
 
-int postprocess() {
+void postprocess(){
     std::cout << "This is the postprocess source" << std::endl;
-    writevth();
-    return 0;
 }
+
+// Recursive variadic template function
+template<typename T, typename... Args>
+void postprocess(T first, Args... args) {
+    //Giro::Solve msv;
+    //int ind = msv.matchscalartovar(first);
+    writevth();
+    postprocess(args...);
+}
+
+// Explicit instantiation to allow separate compilation
+template void postprocess<>(const char* first);
+template void postprocess<>(std::string first);
+template void postprocess<>(const char* first, const char* second);
+template void postprocess<>(std::string first, std::string second);
