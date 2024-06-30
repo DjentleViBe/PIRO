93268cf416a91a5643ce96b49a2f618562948da5
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-07-01 01:21:01 +0200

    setbc

--- src/solve.cpp
+++ src/solve.cpp
@@ -1,3 +1,4 @@
+#include "../dependencies/include/bc.hpp"
 #include "../dependencies/include/solve.hpp"
 #include "../dependencies/include/extras.hpp"
 #include "../dependencies/include/operatoroverload.hpp"
@@ -5,11 +6,11 @@
 
 int solve(){
 
-    // apply boundary conditions
-
     // solve equations
     Giro::Solve solver;
     MP.AMR[0].CD[0].values = solver.ddt_r("T") - solver.ddc_r("Alpha", 1.0) * solver.laplacian_r("T");
+    // apply boundary conditions
+    setbc();
     // update timestep
     return 0;
 }
