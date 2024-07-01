7f6d0c47dd86a6d2be7b151039450f6dd2d44990
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-07-01 23:15:22 +0200

    comment

--- src/solve.cpp
+++ src/solve.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int solve(){
-
+    std::cout << "Solving . . ." << std::endl;
     // solve equations
     Giro::Solve solver;
     MP.AMR[0].CD[0].values = solver.ddt_r("T") - solver.ddc_r("Alpha", 1.0) * solver.laplacian_r("T");
