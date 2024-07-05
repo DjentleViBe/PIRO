13ef21909594fdec5afc61a83408a8b1255af7f4
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-07-06 01:56:09 +0200

    directory logic

--- src/init.cpp
+++ src/init.cpp
@@ -2,21 +2,18 @@
 #include <filesystem>
 #include "../dependencies/include/init.hpp"
 #include "../dependencies/include/extras.hpp"
+#include "../dependencies/include/solve.hpp"
 
 std::filesystem::path current_path;
 
 void init(){
-    /*try {
-        // Get the current path
-        current_path = std::filesystem::current_path();
-        
-        // Print the current path
-        std::cout << "Current working directory: " << current_path << std::endl;
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }*/
+
     get_exec_directory();
-    create_directory(current_path.string() + "/" + "mesh");
-    create_directory(current_path.string() + "/" + "mesh/level");
+    if(SP.restart){
+        delete_directory(current_path.string() + "/" + SP.casename);
+    }
+    create_directory(current_path.string() + "/" + SP.casename);
+    create_directory(current_path.string() + "/" + SP.casename + "/mesh");
+    create_directory(current_path.string() + "/" + SP.casename + "/mesh/level");
 
 }
