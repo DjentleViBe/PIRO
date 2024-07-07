1ac79767138a5380bdc6c2336dd6e90d3832c7b7
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-07-07 04:42:58 +0200

    idx

--- dependencies/include/preprocess.hpp
+++ dependencies/include/preprocess.hpp
@@ -6,9 +6,10 @@
 
 extern Giro::MeshParams MP;
 extern Giro::SolveParams SP;
-extern std::vector<std::vector<float>> scadivmatrix, vecdivmatrix, scalapmatrix, veclapmatrix;
+extern std::vector<std::vector<float>> scagradmatrix, scadivmatrix, scalapmatrix;
 extern int ts;
 
 int preprocess();
+int idx(int i, int j, int k, int N_x, int N_y);
 
 #endif
