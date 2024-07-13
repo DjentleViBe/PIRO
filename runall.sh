c735195440cbed5b95c09ec0d731737abf9b18c4
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2024-07-13 09:59:43 +0200

    run script for different os

--- runall.sh
+++ runall.sh
@@ -1,3 +1,27 @@
+extension=""
+
+detect_os() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "Operating System: Linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Operating System: macOS"
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
+        echo "Operating System: Cygwin (POSIX compatibility layer on Windows)"
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        extension=".exe"
+        echo "Operating System: MinGW (Minimal GNU for Windows)"
+    elif [[ "$OSTYPE" == "win32" ]]; then
+        echo "Operating System: Windows"
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        echo "Operating System: FreeBSD"
+    else
+        echo "Operating System: Unknown"
+    fi
+}
+
+detect_os
+
 cp -r ./dependencies/assets ./bin/.
 mkdir -p logs
-./bin/GIRO &> ./logs/output.txt
+
+./bin/GIRO$extension &> ./logs/output.txt
