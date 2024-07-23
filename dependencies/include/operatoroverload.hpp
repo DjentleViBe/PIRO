06132ed6e888a8fdb103522b3b522a390b58c039
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-07-23 22:45:45 +0200

    operatoroverload

--- dependencies/include/operatoroverload.hpp
+++ dependencies/include/operatoroverload.hpp
@@ -2,6 +2,13 @@
 
 #ifndef OPERATOROVERLOADHPP
 #define OPERATOROVERLOADHPP
+#ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+#elif _WIN32
+    #include "../dependencies/include/CL/opencl.h"
+#else
+    #include "../dependencies/include/CL/opencl.h"
+#endif
 
 #include <vector>
 
@@ -9,8 +16,14 @@ std::vector<float> operator+(const std::vector<float> &v1, const std::vector<flo
 std::vector<std::vector<float>> operator+(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
 std::vector<float> operator-(const std::vector<float> &v1, const std::vector<float> &v2);
 std::vector<std::vector<float>> operator-(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
-std::vector<float> operator*(const std::vector<float> &v1, const std::vector<float> &v2);
-std::vector<std::vector<float>> operator*(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
-std::vector<float> operator/(const std::vector<float> &v1, const std::vector<float> &v2);
-std::vector<std::vector<float>> operator/(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+//std::vector<float> operator*(const std::vector<float> &v1, const std::vector<float> &v2);
+
+
+
+
+
+cl_mem operator*(cl_mem partA, cl_mem partB);
+//std::vector<std::vector<float>> operator*(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
+//std::vector<float> operator/(const std::vector<float> &v1, const std::vector<float> &v2);
+//std::vector<std::vector<float>> operator/(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2);
 #endif // OPERATOR_OVERLOAD_HPP
--- src/operatoroverload.cpp
+++ src/operatoroverload.cpp
@@ -1,5 +1,13 @@
+#ifdef __APPLE__
+    #include <OpenCL/opencl.h>
+#elif _WIN32
+    #include "../dependencies/include/CL/opencl.h"
+#else
+    #include "../dependencies/include/CL/opencl.h"
+#endif
 #include <vector>
 #include "../dependencies/include/solve.hpp"
+#include "../dependencies/include/preprocess.hpp"
 #include "../dependencies/include/operatoroverload.hpp"
 // Overloading the + operator for std::vector<float>
 std::vector<float> operator+(const std::vector<float> &v1, const std::vector<float> &v2) {
@@ -22,16 +30,25 @@ std::vector<std::vector<float>> operator-(const std::vector<std::vector<float>> 
     return subM.subtractMatrices(v1, v2);
 }
 
-std::vector<float> operator*(const std::vector<float> &v1, const std::vector<float> &v2) {
-    Giro::MathOperations mul;
-    return mul.multiplyVectors(v1, v2);
+
+cl_mem operator*(cl_mem partA, cl_mem partB) {
+    cl_int err;
+    int N = MP.n[0] * MP.n[1] * MP.n[2];
+    size_t globalWorkSize[1] = { (size_t)N };
+    err |= clSetKernelArg(kernel_multiplyVec, 0, sizeof(cl_mem), &partA);
+    err |= clSetKernelArg(kernel_multiplyVec, 1, sizeof(cl_mem), &partB);
+    err = clEnqueueNDRangeKernel(queue, kernel_multiplyVec, 1, NULL, globalWorkSize, NULL, 0, NULL, NULL);
+    
+    return partA;
 }
 
+/*
 std::vector<std::vector<float>> operator*(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2) {
     Giro::MathOperations mulM;
     return mulM.multiplyMatrices(v1, v2);
-}
+}*/
 
+/*
 std::vector<float> operator/(const std::vector<float> &v1, const std::vector<float> &v2) {
     Giro::MathOperations div;
     return div.divideVectors(v1, v2);
@@ -40,4 +57,4 @@ std::vector<float> operator/(const std::vector<float> &v1, const std::vector<flo
 std::vector<std::vector<float>> operator/(const std::vector<std::vector<float>> &v1, const std::vector<std::vector<float>> &v2) {
     Giro::MathOperations divM;
     return divM.divideMatrices(v1, v2);
-}
+}*/
