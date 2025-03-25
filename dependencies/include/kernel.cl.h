2196618807246252e65ac682b3b022c75ba531ff
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-03-25 22:28:56 +0100

    N = 8 and loop outside kernel

--- dependencies/include/kernel.cl.h
+++ dependencies/include/kernel.cl.h
@@ -396,21 +396,21 @@ __kernel void divideVectors_constant(__global float *A,
 const char *lu_decompose_dense = R"CLC(
 __kernel void lu_decompose_dense(__global float* A, 
                         __global float* L,
-                        int N) {
+                        int N,
+                        int k) {
     // Get the global row index
     unsigned int i = get_global_id(0);
     unsigned int j = get_global_id(1);
 
    if (i < N && j < N) {
         // Forward elimination
-        for (int k = 0; k < N - 1; k++) {
-            float pivot = A[k * N + k];                             // Pivot element at A[k, k]
-            float factor = A[i * N + k] / pivot;                    // Calculate factor to eliminate
-            if (factor != 0){
-                if (i > k && j < N) {                               // Only update rows below the diagonal
-                    if (i > k) {                                    // Process rows below the diagonal
-                        A[i * N + j] -= factor * A[k * N + j];      // Subtract row k from row i
-                    }
+        float pivot = A[k * N + k];  
+        // printf("%f ", pivot);                           // Pivot element at A[k, k]
+        float factor = A[i * N + k] / pivot;                    // Calculate factor to eliminate
+        if (factor != 0){
+            if (i > k && j < N) {                               // Only update rows below the diagonal
+                if (i > k) {                                    // Process rows below the diagonal
+                    A[i * N + j] -= factor * A[k * N + j];      // Subtract row k from row i
                 }
             }
         }
