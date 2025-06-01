9fb7a48717d0fcbcf05f59fab6ee7fc9791d3e35
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-06-02 01:18:22 +0200

    whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,23 @@
 
 int main(int argc, char* argv[])
 {	
-	if (argc != 3) {
+    if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <name>" << std::endl;
         return 1;
     }
     
     std::string name = argv[1];
-	Piro::kernels& kernel = Piro::kernels::getInstance();
-	kernel.setvalue(Piro::kernels::COMPILE, std::stoi(argv[2]));
+    Piro::kernels& kernel = Piro::kernels::getInstance();
+    kernel.setvalue(Piro::kernels::COMPILE, std::stoi(argv[2]));
 
-	Piro::logger::info("Start Program");
-	Piro::file_utilities::get_exec_directory();
-	Piro::preprocess(name);
-	if(kernel.getvalue<int>(Piro::kernels::COMPILE) == 0){
-		Piro::solve();
-	}
+    Piro::logger::info("Start Program");
+    Piro::file_utilities::get_exec_directory();
+    Piro::preprocess(name);
+    if(kernel.getvalue<int>(Piro::kernels::COMPILE) == 0){
+        Piro::solve();
+    }
 
-	Piro::logger::info("End Program");
-	// postprocess("T");
-	return 0;
+    Piro::logger::info("End Program");
+    // postprocess("T");
+    return 0;
 }
