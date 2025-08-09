e475de54a73d1788af1f3a3c34a9c163223f0c14
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-08-10 01:48:53 +0200

    run device script

--- rundevice.sh
+++ rundevice.sh
@@ -1,3 +1,4 @@
 echo "Please note down the Device and Platform number of the hardware where the simulation is intended to be run."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ./scripts/linux/PIRO_devices_LIN
+fi
