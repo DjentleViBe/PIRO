a49becaccfee46eb6af9e2a992fc84c7c9645d60
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-08-10 01:27:01 +0200

    make device windows

--- makedevice.sh
+++ makedevice.sh
@@ -1,5 +1,12 @@
-make -f Makefile.devices clean
-make -f Makefile.devices
-make -f Makefile.devices setup
-
-chmod +x ./scripts/linux/PIRO_devices_LIN
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make -f Makefile.devices clean
+    make -f Makefile.devices
+    make -f Makefile.devices setup
+    chmod +x ./scripts/linux/PIRO_devices_LIN
+elif [[ "$OSTYPE" == "msys" ]]; then
+    NEWPATH="/c/cygwin64/bin"
+    export PATH="$NEWPATH:$PATH"
+    make -f Makefile.devices clean
+    make -f Makefile.devices
+    make -f Makefile.devices setup
+fi
--- rundevice.sh
+++ rundevice.sh
@@ -1,2 +1,3 @@
 echo "Please note down the Device and Platform number of the hardware where the simulation is intended to be run."
-./scripts/linux/PIRO_devices_LIN
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ./scripts/linux/PIRO_devices_LIN
