92e8b96f1d7b90b9de2e57dacf2a0a489d01e1fd
DjentleVibe <116117854+DjentleViBe@users.noreply.github.com>
2025-08-11 08:00:34 +0200

    fedora nvidia

--- scripts/linux/Piro_requirements_build.sh
+++ scripts/linux/Piro_requirements_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-tools_ubuntu=("wget" "git=1:2.34.1-1ubuntu1.15" "make=4.3-4.1build1" "gcc=4:11.2.0-1ubuntu1" "g++=4:11.2.0-1ubuntu1" "clinfo=clinfo=3.0.21.02.21-1" "rsync" "pciutils" "ocl-icd-opencl-dev=2.2.14-3" "pocl-opencl-icd=1.8-3")
+tools_ubuntu_2204=("wget" "git=1:2.34.1-1ubuntu1.15" "make=4.3-4.1build1" "gcc=4:11.2.0-1ubuntu1" "g++=4:11.2.0-1ubuntu1" "clinfo=3.0.21.02.21-1" "rsync" "pciutils" "ocl-icd-opencl-dev=2.2.14-3" "pocl-opencl-icd=1.8-3")
+tools_ubuntu=("wget" "git" "make" "gcc" "g++" "clinfo" "rsync" "pciutils" "ocl-icd-opencl-dev" "pocl-opencl-icd")
 tools_suse=("wget" "git-2.50.1-2.1" "make-4.4.1-3.3" "which" "gcc14" "gcc14-c++" "clinfo-3.0.25.02.14-1.1" "rsync" "pciutils" "ocl-icd-devel-2.3.3-1.1" "pocl-7.0-1.1" "pocl-devel-7.0-1.1")
-tools_fedora=("wget" "git-2.50.1-1.fc42" "which" "make-1:4.4.1-10.fc42" "gcc14" "gcc14-c++" "clinfo-3.0.23.01.25-7.fc42" "rsync" "pciutils" "opencl-headers-3.0-32.20241023git4ea6df1.fc42" "pocl-6.0-6.fc42")
+tools_suse_generic=("wget" "git" "make" "which" "gcc14" "gcc14-c++" "clinfo" "rsync" "pciutils" "ocl-icd-devel" "pocl" "pocl-devel")
+tools_fedora_42=("wget" "git-2.50.1-1.fc42" "which" "make-1:4.4.1-10.fc42" "gcc14" "gcc14-c++" "clinfo-3.0.23.01.25-7.fc42" "rsync" "pciutils" "opencl-headers-3.0-32.20241023git4ea6df1.fc42" "pocl-6.0-6.fc42")
+tools_fedora_generic=("wget" "git" "which" "make" "gcc14" "gcc14-c++" "clinfo" "rsync" "pciutils" "opencl-headers" "pocl")
 tools_arch=("wget" "git" "make" "gcc" "g++" "clinfo" "rsync" "pciutils" "ocl-icd")
 
 if command -v sudo &>/dev/null; then
@@ -11,19 +14,25 @@ else
     SUDO=""
 fi
 
+# Run as root check
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root: sudo $0"
+    exit 1
+fi
+
 missing_tools=()
 echo "Checking commands..."
 
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
+
 check_tools() {
     local -n tools_ref=$1
     local -n missing_ref=$2
     missing_ref=()
 
     for tool in "${tools_ref[@]}"; do
-        # Strip everything after the first '=' to get the command name
         if [[ "$tool" == *"="* ]]; then
             cmd="${tool%%=*}"       # Debian/Ubuntu
         elif [[ "$tool" =~ -[0-9] ]]; then
@@ -46,44 +55,53 @@ check_tools() {
     fi
 }
 
-# Run as root check
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root: sudo $0"
-    exit 1
-fi
-
 if [ -f /etc/debian_version ]; then
     echo "Debian/Ubuntu detected."
     apt update
-    check_tools tools_ubuntu missing_tools
+    OS_VERSION=$(grep '^VERSION=' /etc/os-release | cut -d'"' -f2)
+    if [[ "$OS_VERSION" == 22.04* ]]; then
+        check_tools tools_ubuntu_2204 missing_tools
+    else
+        check_tools tools_ubuntu_generic missing_tools
+    fi
     for t in "${missing_tools[@]}"; do
         apt install -y "$t"
     done
 
-    # Optional: NVIDIA OpenCL runtime if NVIDIA GPU detected
     if lspci | grep -i nvidia > /dev/null; then
         echo "NVIDIA GPU detected, installing NVIDIA OpenCL ICD..."
-        # apt install -y libnvidia-compute-570
+        DRIVER_VERSION=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | head -n1)
+        DRIVER_MAJOR=$(echo "$DRIVER_VERSION" | cut -d. -f1)
+        apt install -y "libnvidia-compute-${DRIVER_MAJOR}"
     fi
 
 elif [ -f /etc/redhat-release ]; then
     echo "Fedora detected."
     yum update -y
-    check_tools tools_fedora missing_tools
+    OS_VERSION=$(grep '^VERSION_ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+    if [[ "$OS_VERSION" == "42" ]]; then
+        check_tools tools_fedora_42 missing_tools
+    else
+        check_tools tools_fedora_generic missing_tools
+    fi
     for t in "${missing_tools[@]}"; do
         yum install --assumeyes "$t"
     done
     $SUDO ln -s $(which g++-14) /usr/bin/g++
     $SUDO ln -sf /lib64/libOpenCL.so.1 /lib64/libOpenCL.so
-
     if lspci | grep -i nvidia > /dev/null; then
         echo "NVIDIA GPU detected, installing NVIDIA OpenCL..."
-        # yum install -y opencl-nvidia
+        $SUDO dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+        $SUDO dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+        dnf install -y --allowerasing xorg-x11-drv-nvidia-cuda
+    elif lspci | grep -i amd > /dev/null; then
+        echo "AMD GPU detected, installing AMD OpenCL..."
+        yum install -y rocm-opencl
     fi
 
 elif [ -f /etc/arch-release ]; then
     echo "Arch Linux detected."
-     check_tools tools_arch missing_tools
+    check_tools tools_arch missing_tools
     for t in "${missing_tools[@]}"; do
         pacman -Sy --noconfirm "$t"
     done
--- scripts/linux/Piro_requirements_run.sh
+++ scripts/linux/Piro_requirements_run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-tools_ubuntu=("wget" "git=1:2.34.1-1ubuntu1.15" "clinfo=3.0.21.02.21-1" "rsync" "pciutils"  "ocl-icd-opencl-dev=2.2.14-3" "pocl-opencl-icd=1.8-3")
+tools_ubuntu_2204=("wget" "git=1:2.34.1-1ubuntu1.15" "clinfo=3.0.21.02.21-1" "rsync" "pciutils" "ocl-icd-opencl-dev=2.2.14-3" "pocl-opencl-icd=1.8-3")
+tools_ubuntu_generic=("wget" "git" "clinfo" "rsync" "pciutils"  "ocl-icd-opencl-dev" "pocl-opencl-icd")
 tools_suse=("wget" "git-2.50.1-2.1" "which" "rsync" "pciutils" "clinfo-3.0.25.02.14-1.1" "ocl-icd-devel-2.3.3-1.1" "pocl-7.0-1.1" "pocl-devel-7.0-1.1")
-tools_fedora=("wget" "git-2.50.1-1.fc42" "which" "clinfo-3.0.23.01.25-7.fc42" "rsync" "pciutils" "opencl-headers-3.0-32.20241023git4ea6df1.fc42" "pocl-6.0-6.fc42")
+tools_suse_generic=("wget" "git" "which" "rsync" "pciutils" "clinfo" "ocl-icd-devel" "pocl" "pocl-devel")
+tools_fedora_42=("wget" "git-2.50.1-1.fc42" "which" "clinfo-3.0.23.01.25-7.fc42" "rsync" "pciutils" "opencl-headers-3.0-32.20241023git4ea6df1.fc42" "pocl-6.0-6.fc42")
+tools_fedora_generic=("wget" "git" "which" "clinfo" "rsync" "pciutils" "opencl-headers" "pocl")
 tools_arch=("wget" "git" "clinfo" "rsync" "pciutils" "ocl-icd" "pocl")
 
 if command -v sudo &>/dev/null; then
@@ -19,6 +22,7 @@ fi
 
 missing_tools=()
 echo "Checking commands..."
+
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
@@ -54,7 +58,12 @@ check_tools() {
 if [ -f /etc/debian_version ]; then
     echo "Debian/Ubuntu detected."
     apt update
-    check_tools tools_ubuntu missing_tools
+    OS_VERSION=$(grep '^VERSION=' /etc/os-release | cut -d'"' -f2)
+    if [[ "$OS_VERSION" == 22.04* ]]; then
+        check_tools tools_ubuntu_2204 missing_tools
+    else
+        check_tools tools_ubuntu_generic missing_tools
+    fi
     for t in "${missing_tools[@]}"; do
         apt install -y "$t"
     done
@@ -69,14 +78,25 @@ if [ -f /etc/debian_version ]; then
 elif [ -f /etc/redhat-release ]; then
     echo "Red Hat/CentOS/Fedora detected."
     yum update -y
-    check_tools tools_fedora missing_tools
+    OS_VERSION=$(grep '^VERSION_ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+    if [[ "$OS_VERSION" == "42" ]]; then
+        check_tools tools_fedora_42 missing_tools
+    else
+        check_tools tools_fedora_generic missing_tools
+    fi
     for t in "${missing_tools[@]}"; do
         yum install -y "$t"
     done
-
+    $SUDO ln -s $(which g++-14) /usr/bin/g++
+    $SUDO ln -sf /lib64/libOpenCL.so.1 /lib64/libOpenCL.so
     if lspci | grep -i nvidia > /dev/null; then
         echo "NVIDIA GPU detected, installing NVIDIA OpenCL..."
-        # yum install -y opencl-nvidia
+        $SUDO dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+        $SUDO dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+        dnf install -y --allowerasing xorg-x11-drv-nvidia-cuda
+    elif lspci | grep -i amd > /dev/null; then
+        echo "AMD GPU detected, installing AMD OpenCL..."
+        yum install -y rocm-opencl
     fi
 
 elif [ -f /etc/arch-release ]; then
@@ -133,7 +153,7 @@ elif [ -f /etc/SuSE-release ] || grep -qi "opensuse" /etc/os-release; then
     for t in "${missing_tools[@]}"; do
         zypper install -y "$t"
     done
-
+    $SUDO ln -s $(which g++-14) /usr/bin/g++
     if lspci | grep -i nvidia > /dev/null; then
         echo "NVIDIA GPU detected, installing NVIDIA OpenCL..."
         # zypper install -y opencl-nvidia
